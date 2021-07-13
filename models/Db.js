c4ecdf8ba88e22281b050217ea35de253eda362f
Meer Sawood <msawood@redhat.com>
2021-07-13 20:23:28 +0530

    added files

--- models/Db.js
+++ models/Db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const DBURL =
-  "mongodb+srv://admin:admin@mycluster.l4tgf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "";
 var db = mongoose.connect(DBURL)
 .then(() => console.log('mongoose up'))
 
